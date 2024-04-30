cda71754e146494964d998d2bb86917caa25f0cd
Novejira Angela <156919887+Novejira@users.noreply.github.com>
2024-04-30 13:28:25 +0700

    add login , register and splash

---
+++ app/src/main/java/com/nafi/cryptospy/presentation/login/LoginViewModel.kt
@@ -0,0 +1,28 @@
+package com.nafi.cryptospy.presentation.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nafi.cryptospy.data.repository.UserRepository
+import com.nafi.sfocryptospyods.utils.ResultWrapper
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class LoginViewModel (private val repository: UserRepository) : ViewModel() {
+
+    private val _loginResult = MutableLiveData<ResultWrapper<Boolean>>()
+    val loginResult: LiveData<ResultWrapper<Boolean>>
+    get() = _loginResult
+
+    fun doLogin(
+        email: String,
+        password: String,
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.doLogin(email, password).collect {
+                _loginResult.postValue(it)
+            }
+        }
+    }
+}
